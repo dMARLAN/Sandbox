ad7485b390dca89612175eedb4c4f596879090d6
MARLAN <cpenarsky@shaw.ca>
2022-11-03 21:08:02 -0500

    add solutions

---
+++ InterviewQuestions/src/palindromenumber/Solution.java
@@ -0,0 +1,35 @@
+package palindromenumber;
+
+public class Solution {
+    public static void main(String[] args) throws AssertionError {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
+
+    public static boolean isPalindrome(int x){
+        if (x < 0) {
+            return false;
+        }
+        String s = String.valueOf(x);
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome2(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
---
+++ InterviewQuestions/src/validpalindrome/Solution.java
@@ -0,0 +1,20 @@
+package validpalindrome;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // Expected: true
+        System.out.println(isPalindrome("race a car")); // Expected: false
+        System.out.println(isPalindrome("0z;z   ; 0")); // Expected: true
+    }
+
+    public static boolean isPalindrome(String s) {
+        char[] c = s.toCharArray();
+        for (int i = 0, j = c.length - 1; i < j; ) {
+            if (!Character.isLetterOrDigit(c[i])) i++;
+            else if (!Character.isLetterOrDigit(c[j])) j--;
+            else if (Character.toLowerCase(c[i++]) != Character.toLowerCase(c[j--]))
+                return false;
+        }
+        return true;
+    }
+}
